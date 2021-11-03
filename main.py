9d21115490f84d7307b8bc79e2d175906e7c2b13
Grupo6-PI <2008259@aluno.univesp.br>
2021-11-03 19:08:23 -0300

    primeiro commit

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def admin():
         cur = conn.cursor()
         cur.execute('SELECT categoryId, name FROM categories')
         categories = cur.fetchall()
-    #   conn.close() 
+    conn.close() 
     return render_template('add.html', categories=categories)
 
 @app.route('/addItem', methods=["GET", "POST"])
